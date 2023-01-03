7eacd150e7384bb0039696349849f0fd506c3f48
Scoozy <s.safanovich@gmail.com>
2023-01-02 21:21:48 -0800

    assa

--- my-app/src/Maincontent/Maincontent.jsx
+++ my-app/src/Maincontent/Maincontent.jsx
@@ -34,10 +34,21 @@ function ContainerFluid() {
             </ul>
           </Col>
       </Row>
-      <Row>
+      <Row className='teamhistory'>
+      <h2 className='H2' id='history'>Team History</h2>
+
+        <div className='hl'></div>
+        
+        
+        <div className="col1">
+          
         <h2 className='subheading' id='history'>Team History</h2>
-        <hr className='hr'></hr>
+        <div className='vl'></div>
         <p className='text'>Our team competes in the FIRST robotics competition as Team 1515. Team 1515 was founded in the fall of 2004 under the leadership of Former Principal of Beverly Hills High School Dr. Dan Stepenosky, member of the School Board Dr. Myra Demeter, Beverly Hills High School Chemistry Teacher Charles Lee, and parent Eileen Kahn. Under their leadership, Team 1515 grew to become a well-known team. Our team continued to flourish with the help of parent Eileen Kahn, who became a credentialed teacher to keep the program in existence. Thanks to the strong leadership and dedication of our teacher, John Castle, our team is moving full S.T.E.A.M ahead. With new student leadership, more efficient subdivisions, and fresh thinking, MorTorq is reaching for the stars. Currently, we have 42 students enrolled, along with many other affiliated with our robotics business team. In 2019, we had the honor to be awarded the prestigious Chairman's Award and traveled to the FRC International Competition with our registration fee sponsored by NASA. The 2022-2023 FIRST season will be our team’s ninteenth year competing.</p>
+
+        </div>
+        
+        
       </Row>
     </Container>
 
