be5b17b97814c8ff1c61b8fde16d5e7b06db57a2
Scoozy <s.safanovich@gmail.com>
2023-01-04 12:46:47 -0800

    cool

--- my-app/src/Maincontent/Maincontent.jsx
+++ my-app/src/Maincontent/Maincontent.jsx
@@ -12,8 +12,6 @@ function reveal() {
 
     if (elementTop < windowHeight - elementVisible) {
       reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
     }
   }
 }
