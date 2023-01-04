3389a094d2ab450f60d7fa26ea8746c36e93d62f
Scoozy <s.safanovich@gmail.com>
2023-01-04 14:39:36 -0800

    new navbar again

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -2,20 +2,29 @@
 import "./navbar.css"
 import pic from "../images/MorTorq.png"
 
+let hamburger = document.querySelector(".hamburger")
+hamburger.onclick = ()=>{
+    navBar = document.querySelector(".nav-bar")
+    navBar.classList.toggle("active")
+}
 function ColorSchemesExample() {
   return (
-    <header class="nav-header">
-      <h3 class="name">Mor<span class="torq">Torq</span></h3>
-      
-<nav>
-    <ul class="nav-links">
-        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Home</a></li>
-        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">About</a></li>
-        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Sponsors</a></li>
-        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Donate</a></li>
-    </ul>
-</nav>
-  <img src={pic} alt="logo" class="logo" />
+    <header>
+    <div class="logo"><img src={pic} alt=""></div>
+    <div class="hamburger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </div>
+    <nav class="nav-bar">
+        <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Sponsors</a></li>
+            <li><a href="#">Donate</a></li>
+
+        </ul>
+    </nav>
 </header>
   )
 }
