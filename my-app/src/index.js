7f7f2e280eab18e85ca8e369732913b7460dd85c
Scoozy <s.safanovich@gmail.com>
2023-01-04 13:40:56 -0800

    some sorting

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from "./Navbar/Navbar.jsx"
-import Maincontent from "./Maincontent/Maincontent.jsx"
-import "../src/Maincontent/Maincontent.css"
+import Navbar from "./components/Navbar/Navbar.jsx"
+import Maincontent from "./components/Maincontent/Maincontent.jsx"
+import "./components/Maincontent/Maincontent.css"
 import Footer from './Footer/Footer';
 import Header from './header/header';
+
 
 function App(){
   return(
@@ -19,15 +20,9 @@ function App(){
     
   )
 }
-const page = (
-  <body>
-      <App/>
-  </body>
-  
-  )
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  page
+  <App/>
 );
 
 
