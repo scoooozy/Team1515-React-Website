1b7fccda9ba1c65d4e2893e9ccdf75c2c233a80a
Scoozy <s.safanovich@gmail.com>
2023-01-03 18:21:11 -0800

    WHY THIS CARD IS NOT WOKING

--- my-app/src/Maincontent/Maincontent.jsx
+++ my-app/src/Maincontent/Maincontent.jsx
@@ -1,27 +1,10 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./Maincontent.css";
 
 function ContainerFluid() {
   return (
     <Container className="container">
-      <Row>
-        {/* <Col>          
-            <ul className='list'>
-                <li className="ulElment">It's made using react js</li>
-                <li className="ulElment">It was only made by me</li>
-                <li className="ulElment">The code is actualy readable</li>
-            </ul>
-          </Col>
-          <Col>          
-            <ul className='list'>
-                <li className="ulElment">The names of the classNames are actualy understandable</li>
-                <li className="ulElment">The load time is faster</li>
-                <li className="ulElment">This website will look better</li>
-            </ul>
-          </Col> */}
-      </Row>
       <Row className="teamhistory">
         <h2 className="H2" id="history">
           Team History
@@ -30,7 +13,7 @@ function ContainerFluid() {
         <div className="hl"></div>
 
         <div className="col1">
-          <h2 className="subheading" id="history">
+          <h2 className="vheading" id="history">
             Team History
           </h2>
           <div className="vl"></div>
@@ -55,63 +38,74 @@ function ContainerFluid() {
           </p>
         </div>
       </Row>
-      <div className="robot-section">
-        <h2 className="robotheading">Our Robots</h2>
-        <Row>
-          <Col>
-            <div class="card">
-              <div class="card-content">
-                <h2 class="card-title">Rapid React </h2>
-                <h4>2022</h4>
-                <p class="card-body">
-                  In the 2022 season, our team participated in the LA and
-                  Ventura regionals and won up to quarter finals in both
-                  tournaments. We were twice awarded the FRC Imagery Award for
-                  outstanding attractiveness in our engineering and visual
-                  aesthetic of both our robot and team appearance.
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                </p>
-              </div>
+      <section className="robot-section">
+        <h1 className="robotheading">Our Robots</h1>
+        <div className="hl"></div>
+        <div className="card-container">
+          <div class="card">
+            <div class="card-content">
+              <h2 class="card-title">Rapid React </h2>
+              <h4>2022</h4>
+              <p class="card-body">
+                In the 2022 season, our team participated in the LA and Ventura
+                regionals and won up to quarter finals in both tournaments. We
+                were twice awarded the FRC Imagery Award for outstanding
+                attractiveness in our engineering and visual aesthetic of both
+                our robot and team appearance.
+                <br></br>
+                <br></br>
+                <br></br>
+              </p>
             </div>
-            <div class="card">
-              <div class="card-content">
-                <h2 class="card-title">Infinite Recharge</h2>
-                <h4>2020</h4>
-                <p class="card-body">
-                  Our 2020 robot had a revolver type magazine, a feature that
-                  allowed us to carry five balls at a time. The robot could
-                  intake balls, place them into the magazine, and then used a
-                  solenoid to direct the ball into the two wheel shooter. It
-                  also had a climber mechanism that utilized a hook. The swerve
-                  drivetrain allowed us to move rapidly and avoid obstacles on
-                  the field.
-                </p>
-              </div>
+          </div>
+          <div class="card">
+            <div class="card-content">
+              <h2 class="card-title">Infinite Recharge</h2>
+              <h4>2020</h4>
+              <p class="card-body">
+                Our 2020 robot had a revolver type magazine, a feature that
+                allowed us to carry five balls at a time. The robot could intake
+                balls, place them into the magazine, and then used a solenoid to
+                direct the ball into the two wheel shooter. It also had a
+                climber mechanism that utilized a hook. The swerve drivetrain
+                allowed us to move rapidly and avoid obstacles on the field.
+              </p>
             </div>
-            <div class="card">
-              <div class="card-content">
-                <h2 class="card-title">Destination Deepspace  </h2>
-                <h4>2019</h4>
-                <p class="card-body">
-                  In 2019, our team competed at the Ventura Regional, the LA
-                  Regional, and won the Chairmans Award.
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  <br></br>
-                  
-                </p>
-              </div>
+          </div>
+          <div class="card">
+            <div class="card-content">
+              <h2 class="card-title">Destination Deepspace </h2>
+              <h4>2019</h4>
+              <p class="card-body">
+                In 2019, our team competed at the Ventura Regional, the LA
+                Regional, and won the Chairmans Award.
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+              </p>
             </div>
-          </Col>
-        </Row>
-      </div>
+          </div>
+        </div>
+      </section>
+      <section className="previous-comp">
+        <h1>Previous Competitions</h1>
+        <div className="hl"></div>
+        <div className="info-card">
+            <img src="./ventura.jpg" alt="" className="info-image" />
+          <div className="content">
+            <h2 className="heading">FRC-Rapid React</h2>
+            <h3 className="subheading">2022</h3>
+            <p className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea magnam ad saepe sequi dicta reiciendis molestiae alias commodi odit numquam!
+            </p>
+          </div>
+        </div>
+
+      </section>
     </Container>
   );
 }
