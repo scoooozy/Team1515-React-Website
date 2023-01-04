5631ba6a842b0aebc9fba6435196b48531fc77fe
Scoozy <s.safanovich@gmail.com>
2023-01-03 21:41:00 -0800

    pls work

--- my-app/src/Navbar/Navbar.jsx
+++ my-app/src/Navbar/Navbar.jsx
@@ -3,19 +3,19 @@ import "./navbar.css"
 
 function ColorSchemesExample() {
   return (
-    <nav className="page-navbar" data-spy="affix" data-offset-top="10">
-    <ul class="nav-navbar container">
-        <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
-        <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
-        <li class="nav-item"><a href="index.html" class="nav-link"><img src="./images/logo.png" alt="lol" className='logo'/> </a></li>
-        <li class="nav-item"><a href="sponsors.html" class="nav-link">Sponsors</a></li>
-        <li class="nav-item search">
-            <a href="https://www.bhef.org/apps/form/bhhsrobotics" target="_blank" class="nav-link search-toggle"><i class="ti-search"></i> Donate</a>
-            
-        </li>
+    <header class="header">
+    <h3 class="name">MorTorq</h3>
+<nav>
+    <ul class="nav-links">
+        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Home</a></li>
+        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">About</a></li>
+        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Sponsors</a></li>
+        <li class="li"><a href="http://" target="_blank" rel="noopener noreferrer">Donate</a></li>
     </ul>
-  </nav>
-  );
+</nav>
+  <img src="MorTorq.png" alt="logo" class="logo">
+</header>
+  )
 }
 
 export default ColorSchemesExample;
