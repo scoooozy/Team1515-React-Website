791cc488ea6b727477450ed24d4e1bad005d91ed
Scoozy <79184720+scoooozy@users.noreply.github.com>
2023-05-05 15:01:18 -0700

    going pretty well made navbar look ok

--- my-app/src/components/Navbar/Navbar.jsx
+++ my-app/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 function NavPage() {
   return (
     <div>
-      <Navbar bg="light" expand="lg" className="navbg">
+      <Navbar expand="lg"  bg="dark" variant="dark" className="navbg">
         <Container>
           <Navbar.Brand href="#home">
             <img src={pic} alt="logo  " className="logo img" />
@@ -20,23 +20,35 @@ function NavPage() {
             <Nav className="me-auto">
               <Nav.Link href="#home">
                 {" "}
-                <Link className="links" to="/">Home</Link>
+                <Link className="links" to="/">
+                  Home
+                </Link>
               </Nav.Link>
-              <NavDropdown title="About" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
+              <NavDropdown
+                title="About"
+                id="nav-dropdown-dark-example"
+                menuVariant="dark"
+                className="links"
+              >
+                <NavDropdown.Item href="#action/3.1" className="links">
                   Our Leaders
                 </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Outreach</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2" className="links">
+                  Outreach
+                </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.3">Sponsors</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3" className="links">
+                  Sponsors
+                </NavDropdown.Item>
               </NavDropdown>
-              <Nav.Link href="#link">Donate</Nav.Link>
+              <Nav.Link href="#link" className="links">
+                Donate
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <Outlet />
-
     </div>
   );
 }
