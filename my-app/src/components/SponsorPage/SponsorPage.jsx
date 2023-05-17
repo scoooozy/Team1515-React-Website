46d2d89312f878de62d6a240f889cd1a8b5ffb12
PUKAN <113951267+siksakas@users.noreply.github.com>
2023-05-17 10:38:46 -0700

    z

--- my-app/src/components/SponsorPage/SponsorPage.jsx
+++ my-app/src/components/SponsorPage/SponsorPage.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import NavPage from '../Navbar/Navbar'
-import './SponsorPage.css'
-import GeneHaas from './Gene Haas Logo.png'
-import OhanaCo from './Ohana & Co Logo.png'
-import Footer from '../Footer/Footer'
-import whiteHaas from './Haas White.png'
+import React from "react";
+import NavPage from "../Navbar/Navbar";
+import "./SponsorPage.css";
+import GeneHaas from "./Gene Haas Logo.png";
+import OhanaCo from "./Ohana & Co Logo.png";
+import Footer from "../Footer/Footer";
+import whiteHaas from "./Haas White.png";
+import { Container, Row } from "react-bootstrap";
 
 const SponsorPage = () => {
   return (
     <div>
-      <div className='black'>
-        <NavPage/>
-        <div className='bg-sponsors header'>
-          <h1 className='sptext'>
+      <div className="black">
+        <NavPage />
+        <div className="bg-sponsors header">
+          <h1 className="sptext">
             SPONSORS
-            <h1 className='sptext-sub'>THANK YOU FOR SUPPORTING OUR TEAM</h1>
-            <h1 className='sptext-sub'>SERGEY DONT CHECK THE MOBILE SCREEN VERSION PLS</h1>
+            <h1 className="sptext-sub">THANK YOU FOR SUPPORTING OUR TEAM</h1>
           </h1>
         </div>
-        <div className="sp-photo-grid">
-          <div className="sp-row">
-            <img src={whiteHaas} alt="Gene Haas Foundation" />
-            <img src={OhanaCo} alt="Ohana & Co"/>
-          </div>
-        </div>
-      </div> 
-      <Footer /> 
+          <Container>
+            <Row>
+              <img src={whiteHaas} alt="Gene Haas Foundation" className="sp-img" />
+              <img src={OhanaCo} alt="Ohana & Co"  className="sp-img"/>
+            </Row>
+          </Container>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default SponsorPage
+export default SponsorPage;
